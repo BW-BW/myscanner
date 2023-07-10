7ff8d9725c0938a9bd7856a9de7d7156ae2f52fb
BW-BW <brianwidjaja53@gmail.com>
2023-07-10 12:17:09 +0800

    Tutorial Finish

--- lib/userpages/page_tutorial.dart
+++ lib/userpages/page_tutorial.dart
@@ -19,30 +19,116 @@ class PageIntroState extends State<PageIntro> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Color(0xffebebeb),
         appBar: AppBar(
-          title: const Text('Demo'),
+          elevation: 0,
+          centerTitle: true,
+          automaticallyImplyLeading: false,
+          backgroundColor: Color(0xffffffff),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.zero,
+          ),
+          title: Text(
+            'New User Quick Introduction',
+            style: TextStyle(
+              fontWeight: FontWeight.w700,
+              fontStyle: FontStyle.normal,
+              fontSize: 20,
+              color: Color(0xff000000),
+            ),
+          ),
         ),
-        body: ImageSlideshow(
-          indicatorColor: Colors.blue,
-          onPageChanged: (value) {
-            debugPrint('Page changed: $value');
-          },
-          autoPlayInterval: 3000,
-          isLoop: true,
-          children: [
-            Image.asset(
-              'images/sample_image_1.jpg',
-              fit: BoxFit.cover,
-            ),
-            Image.asset(
-              'images/sample_image_2.jpg',
-              fit: BoxFit.cover,
-            ),
-            Image.asset(
-              'images/sample_image_3.jpg',
-              fit: BoxFit.cover,
-            ),
-          ],
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(10, 20, 10, 5),
+                child: Text(
+                  'Scroll through these images to see the tutorial',
+                  textAlign: TextAlign.start,
+                  maxLines: 1,
+                  overflow: TextOverflow.clip,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    fontStyle: FontStyle.normal,
+                    fontSize: 14,
+                    color: Color(0xff3a57e8),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Container(
+                  padding: EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Color(0xff3a57e8)),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: ImageSlideshow(
+                    height: 700,
+                    indicatorColor: Color(0xff3a57e8),
+                    onPageChanged: (value) {
+                      debugPrint('Page changed: $value');
+                    },
+                    autoPlayInterval: 10000,
+                    isLoop: true,
+                    children: [
+                      Image.asset(
+                        'assets/tutorial/tutorial1.jpg',
+                        fit: BoxFit.cover,
+                      ),
+                      Image.asset(
+                        'assets/tutorial/tutorial2.jpg',
+                        fit: BoxFit.cover,
+                      ),
+                      Image.asset(
+                        'assets/tutorial/tutorial3.jpg',
+                        fit: BoxFit.cover,
+                      ),
+                      Image.asset(
+                        'assets/tutorial/tutorial4.jpg',
+                        fit: BoxFit.cover,
+                      ),
+                      Image.asset(
+                        'assets/tutorial/tutorial5.jpg',
+                        fit: BoxFit.cover,
+                      ),
+                      Image.asset(
+                        'assets/tutorial/tutorial6.jpg',
+                        fit: BoxFit.cover,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(15, 16, 15, 20),
+                child: MaterialButton(
+                  onPressed: () {
+                    Navigator.of(context)
+                        .pushNamedAndRemoveUntil("/user", (route) => false);
+                  },
+                  color: Color(0xff3a57e8),
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  padding: EdgeInsets.all(16),
+                  textColor: Color(0xffffffff),
+                  height: 45,
+                  minWidth: MediaQuery.of(context).size.width,
+                  child: Text(
+                    "I'm Ready",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w700,
+                      fontStyle: FontStyle.normal,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
